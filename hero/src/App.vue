9b0259230ed7e2ca583715ebf1c41c7829123d47
wtfeamnu <os9777190@gmail.com>
2025-08-12 22:36:26 -0300

    FEAT#0: apenas fiz de como linkar o backend com o front e deu certo

---
+++ hero/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import api from './services/api';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      usuarios: [],
+    };
+  },
+  mounted() {
+    // Quando o componente carregar, vamos buscar os usuários
+    api.get('usuarios/')
+      .then(response => {
+        this.usuarios = response.data;
+      })
+      .catch(error => {
+        console.error("Erro ao buscar usuários:", error);
+      });
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Lista de Usuários</h1>
+    <ul>
+      <li v-for="user in usuarios" :key="user.id">
+        {{ user.username }} - {{ user.email }}
+      </li>
+    </ul>
+  </div>
+</template>
+
