8e35d8661613a72f955027a75bebe6d32eab961c
helenasf8 <helenadasilvafortunato.if@gmail.com>
2025-08-14 10:44:26 -0300

    componentes do começo do site

---
+++ hero/src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<template>
+<header>
+    <nav>
+        <h1><!--logo--></h1>
+        <ul>
+            <li>
+                <a href="#">Início</a>
+            </li>
+            <li>
+                <a href="#">Comunidades</a>
+            </li>
+            <li>
+                <a href="#">Loja</a>
+            </li>
+        </ul>
+        <button>Entrar</button>
+    </nav>
+</header>
+</template>
+
+<style scoped>
+</style>
