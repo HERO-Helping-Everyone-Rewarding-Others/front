1c01c2085ee07a61c0bf92ad7835a6247409c553
helenasf8 <helenadasilvafortunato.if@gmail.com>
2025-08-20 23:22:14 -0300

    footer sem cor

--- hero/src/components/FooterComponent.vue
+++ hero/src/components/FooterComponent.vue
@@ -1,10 +1,98 @@
 <script setup>
+    import { CIcon } from '@coreui/icons-vue';
+  import { cibFacebookF, cibLinkedinIn } from '@coreui/icons';
 </script>
 <template>
     <footer>
-
+        <nav>
+            <section class="footer1">
+                <ul>
+                <li class="social">
+                    <a href="https://www.facebook.com/?locale=pt_BR"><CIcon :icon="cibFacebookF" /></a>
+                </li>
+                <li class="social">
+                    <a href="https://x.com/"><span class="mdi mdi-twitter"></span></a>
+                </li>
+                <li class="social">
+                    <a href="https://br.linkedin.com/"><CIcon :icon="cibLinkedinIn" /></a>
+                </li>
+            </ul>
+            <ul>
+                <li class="contato">
+                    <span class="mdi mdi-phone"></span>
+                    <p>
+                        + 123-456-789
+                    </p>
+                </li>
+                <li class="contato">
+                    <span class="mdi mdi-email"></span>
+                    <p>
+                        info@gmail.com
+                    </p>
+                </li>
+            </ul>
+            <div class="logo-footer">
+                    <img src="/he(1).png" alt="logo">
+                </div>
+            </section>
+            <section class="footer2">
+                    <p>
+                        Copyright &copy;2025
+                    </p>
+            </section>
+        </nav>
     </footer>
 </template>
 
 <style scoped>
+    footer {
+        margin: 25vw 0 1vw 0;
+        padding: 5vw 0 0 0;
+        background-color: #cacbcc;
+        text-align: center;
+    }
+    footer nav section.footer1 {
+        text-align: center;
+    }
+    .logo-footer img {
+        height: 70px;
+        width: auto;
+        margin: 1vw 0 3vw 0;
+    }   
+    footer nav ul {
+        display: flex;
+        justify-content: center;
+    }
+    footer nav ul li {
+        list-style: none;
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        padding: 0 20px;
+    }
+    .social a {
+        color: white;
+        background-color: black;
+        padding: 10px 15px 10px 15px;
+        border-radius: 100%;
+    }
+    .contato {
+        color: white;
+        padding: 0 20px;
+    }
+    .contato span {
+        font-size: 1.5rem;
+        padding: 0 15px;
+    }
+    .contato p {
+        margin: 4px 0;
+        font-size: 1.2rem;     
+    }
+    .footer2 {
+        background-color: rgb(36, 35, 35);
+        padding: 10px 0 1px 0;
+    }
+    .footer2 p {
+        color: white;
+    }
 </style>
