de10eeadacef3e2ad12c3973f0f3871cc7c75ac4
wtfeamnu <os9777190@gmail.com>
2025-08-21 09:59:06 -0300

    FEAT#4: apagando coisas desnecessarias

--- hero/src/App.vue
+++ hero/src/App.vue
@@ -2,17 +2,17 @@
 import { useRoute } from 'vue-router'
 import HeaderComponent from './components/HeaderComponent.vue'
 import FooterComponent from './components/FooterComponent.vue'
-
 const route = useRoute()
 </script>
 
 <template>
   <div>
-    <!-- Só mostra header/footer se NÃO estiver na página de login -->
+
     <HeaderComponent v-if="route.name !== 'login' && route.name !=='register'" />
 
     <RouterView />
 
     <FooterComponent v-if="route.name !== 'login' && route.name !=='register'" />
   </div>
+
 </template>
