bc8c9ca6206c2f48caf2bd6618f7b8bab2ac59e2
helenasf8 <helenadasilvafortunato.if@gmail.com>
2025-08-22 00:57:57 -0300

    uma parte do css do login feita

--- hero/src/components/Login.vue
+++ hero/src/components/Login.vue
@@ -64,14 +64,52 @@ const handleLogin = async () => {
 
 <style scoped>
   section {
-    background: linear-gradient(135deg, #1b2353 0, #942785 50%, #f6d365 100%);
+    background: linear-gradient(to bottom, #1b2353 0%, #942785 75%, #f6d365 100%);
     overflow: auto;
     display: flex;
     height: 100vh;
+    color: rgb(245, 245, 245);
+    padding: 5vw 8vw 5vw 8vw;
   }
   section .box1 .logo img {
-    height: 70px;
-    width: auto; 
+    height: 4vw;
+    width: auto;
+  }
+  .box1 .logo, h1 {
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    font-size: 2rem;
+    gap: 10px;
+    
+  }
+  .box1 h2 {
+    font-weight: 700;
+    font-size: 3rem;
+    width: 70%;
+    margin: 1vw 0;
+    text-shadow: 2px 2px 5px rgba(0,0,0,0.3);
+  }
+  .box1 p {
+    width: 70%;
+    font-weight: 500;
+    font-size: 1.2rem;
+  }
+  .box1 ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    padding: 0;
+  }
+  .box1 ul li {
+    list-style: none;
+    border: 1px solid rgb(255, 255, 255);
+    box-shadow: 1px 1px 10px 5px rgba(17, 17, 17, 0.2);
+    width: auto;
+    text-align: center;
+    border-radius: 5vw;
+    padding: 6px 20px 6px 20px;
+    font-weight: 500;
   }
 </style>
 
